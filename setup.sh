43afe04832254cba332879607abd2005ad439661
tomer malka pinto <tomermalka12@gmail.com>
2025-05-06 19:49:59 +0300

    docker file update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
- import docker
+import docker
  
- list_running_containers():
- for c in client.containers.list():
+list_running_containers():
+for c in client.containers.list():
  print(f"Name: {c.name}, Status: {c.status}")
+
+container = client.containers.run("nginx", detach=True)
+print(container.logs().decode()[:200])
+container.stop()
+container.remove()
+
